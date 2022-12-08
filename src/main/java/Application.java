b4605c606bd616792f58c2614b83a256e7a6e8c0
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-12-08 18:28:03 +0300

    refactor

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,13 @@
 import guru.qa.domain.Car;
 import guru.qa.domain.RaceTrack;
-import guru.qa.service.GuiInterface;
+import guru.qa.service.Interface;
 
 public class Application {
-    public Application(GuiInterface userInterface) {
+    public Application(Interface userInterface) {
         this.userInterface = userInterface;
     }
 
-    private final GuiInterface userInterface;
+    private final Interface userInterface;
 
 
     void run() {
